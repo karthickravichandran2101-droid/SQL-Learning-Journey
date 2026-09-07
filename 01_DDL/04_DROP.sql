6badc9bdbc03b9d51bc227d6365ed2e7042476d4
karthickravichandran2101-droid <karthick.ravichandran2101@gmail.com>
2026-09-07 12:22:26 +0530

    Create 04_DROP.sql
     DROP TABLE DDL example

---
+++ 01_DDL/04_DROP.sql
@@ -0,0 +1,10 @@
+====================================================
+TOPIC: SQL DDL
+COMMAND: DROP
+
+BUSINESS CASE:
+The Employees table is no longer required.
+====================================================
+*/
+
+DROP TABLE Employees;
