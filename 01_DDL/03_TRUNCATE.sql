b675e4e480fcaaed91ae9e1d22cbc363770dbd4a
karthickravichandran2101-droid <karthick.ravichandran2101@gmail.com>
2026-09-07 12:20:14 +0530

    Create 03_TRUNCATE.sql
    TRUNCATE TABLE DDL example

---
+++ 01_DDL/03_TRUNCATE.sql
@@ -0,0 +1,11 @@
+====================================================
+TOPIC: SQL DDL
+COMMAND: TRUNCATE
+
+BUSINESS CASE:
+Remove all employee records while keeping
+the Employees table structure.
+====================================================
+*/
+
+TRUNCATE TABLE Employees;
