e5c1455afd1737b98a35c3687f19c8176b8c2176
karthickravichandran2101-droid <karthick.ravichandran2101@gmail.com>
2026-09-15 07:03:12 +0530

    Update 01_Equal_To.sql
    Equal To.sql

--- 05_Operators/06_Comparison_Operators/01_Equal_To.sql
+++ 05_Operators/06_Comparison_Operators/01_Equal_To.sql
@@ -1,1 +1,31 @@
+/*
+=========================================================
+PROJECT: SQL Learning Portfolio
+TOPIC: Operators - Comparison Operators
+SCENARIO: Equal To (=)
+DATABASE: MS SQL Server
+=========================================================
 
+BUSINESS REQUIREMENT:
+
+The Sales team wants to identify products whose
+Unit Price is exactly 2000.
+
+The Equal To (=) operator is used to compare whether
+a value exactly matches a specified value.
+*/
+
+
+-- =========================================================
+-- EQUAL TO (=)
+-- =========================================================
+
+SELECT
+    Product_ID,
+    Product_Name,
+    Category,
+    Quantity,
+    Unit_Price,
+    Total_Sales
+FROM SALES_COMPARISON
+WHERE Unit_Price = 2000;
