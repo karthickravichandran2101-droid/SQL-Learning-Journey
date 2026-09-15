938895111ffc965d5d6a41270715a19b71986acd
karthickravichandran2101-droid <karthick.ravichandran2101@gmail.com>
2026-09-15 06:12:32 +0530

    Create 06_Equal_To.sql
    Add Equal To comparison operator

---
+++ 05_Operators/06_Equal_To.sql
@@ -0,0 +1,14 @@
+-- =========================================================
+-- SQL OPERATORS
+-- Topic: Comparison Operators
+-- Operator: Equal To (=)
+-- Database: MS SQL Server
+-- =========================================================
+
+
+-- Example 1:
+-- Find products where Unit_Price is exactly 1500
+
+SELECT *
+FROM Products
+WHERE Unit_Price = 1500;
